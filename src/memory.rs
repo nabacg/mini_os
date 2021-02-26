057dca48915df26d523172e30928b64a1211e713
Grzegorz Caban <nabacg@gmail.com>
2021-02-26 14:02:37 +0000

    No longer used code cleanup

--- src/memory.rs
+++ src/memory.rs
@@ -21,23 +21,6 @@ pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static>
     OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
 
-pub fn create_example_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
-) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
-
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000)); // VGA buffer address
-    let flags = Flags::PRESENT | Flags::WRITABLE;
-
-    let map_to_result = unsafe {
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush(); // flush this page from TLB!
-
-}
-
 
 pub struct EmptyFrameAllocator; // allocator that always returns None
 
